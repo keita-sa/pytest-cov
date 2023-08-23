3734581c1729fd1ea9a6b854c6b1d78f05355b85
Keita Sakurai <keitasakuraims@gmail.com>
2023-08-23 18:06:54 +0900

    Added function

--- test_calculation.py
+++ test_calculation.py
@@ -7,7 +7,19 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = calculation.Cal()
+        cls.test_dir = '/tmp/test_dir'
         cls.test_file_name = 'test.txt'
+
+    def teardown_class(cls):
+        import shutil
+        if os.path.exists(cls.test_dir):
+            shutil.rmtree(cls.test_dir)
+
+    def test_save_no_dir(self):
+        self.cal.save(self.test_dir, self.test_file_name)
+        test_file_path = os.path.join(
+            self.test_dir, self.test_file_name)
+        assert os.path.exists(test_file_path) is True
 
     def test_add_num_and_double(self):
         assert self.cal.add_num_and_double(1, 1) == 4
@@ -17,3 +29,7 @@ class TestCal(object):
         test_file_path = os.path.join(
             tmpdir, self.test_file_name)
         assert os.path.exists(test_file_path) is True
+
+    def test_add_num_and_double_raise(self): # ValueErrorをraise
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double('1', '1')
